4c90c13b39dc57dc6ffe1f210d4eab81eca53dd8
duongnd <77197933+ngduonng@users.noreply.github.com>
2024-11-11 10:49:34 +0700

    new commit

--- DuAnMau_PNLib/app/src/main/java/com/duongnd/quanlythuvien/ui/LoginActivity.java
+++ DuAnMau_PNLib/app/src/main/java/com/duongnd/quanlythuvien/ui/LoginActivity.java
@@ -11,6 +11,9 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.duongnd.quanlythuvien.R;
 import com.duongnd.quanlythuvien.data.dao.NhanVienDAO;
@@ -29,11 +32,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_login), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         sharedPreferences = getSharedPreferences("LoginPrefs", MODE_PRIVATE);
 
